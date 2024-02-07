d23d78951a8c7cfe645e0fb970d97baa3894aa36
SoyLin <145429706+SoyLin@users.noreply.github.com>
2024-02-07 14:14:04 +0100

    Delete Pedidos directory

--- Pedidos/prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cuenta = "AA49997";
-
- if (!preg_match("/^[A-Z]{2}[0-9]{5}$/",$cuenta)) { //comprueba si cumple el formato
-    echo "error: número cuenta no válida";
-}else{//formato válido
-    echo "correcta!";
-}
-?>
